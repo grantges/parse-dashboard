2f079b076dc1ff022871813e04fb4d5dbab214ad
grantges <bert@grantges.co>
2018-04-16 14:58:14 -0500

    adding configuration for Parse Apps

---
+++ parse-dashboard-config.js
@@ -0,0 +1,16 @@
+{
+  "apps": [
+    {
+      "serverURL": "https://my-fly-server.herokuapp.com/parse",
+      "appId": "f77906ce-7daf-474d-8156-ff3cac0f3b35",
+      "masterKey": "f0b58f5b-c24d-4aae-9451-e49f21a063f0 ",
+      "appName": "MyFly Server"
+    }
+  ],
+  "users": [
+    {
+      "user":"grantges",
+      "pass":"t1m1fl1",
+      "apps": [{"appId": "f77906ce-7daf-474d-8156-ff3cac0f3b35"}]
+    }
+}
